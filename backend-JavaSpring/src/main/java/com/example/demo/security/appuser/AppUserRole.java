3e79dde79de12e4ee85368d2d67d32f3a1dc745d
kasluk345 <kaszuba.lukasz@gmail.com>
2021-09-22 17:50:01 +0200

    cleared session security; ready to imlement jwt auth

--- backend-JavaSpring/src/main/java/com/example/demo/security/appuser/AppUserRole.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.security.appuser;
-
-public enum AppUserRole {
-    USER,
-    ADMIN
-}
--- backend-JavaSpring/src/main/java/com/example/demo/security/email/EmailSender.java
+++
@@ -1,5 +0,0 @@
-package com.example.demo.security.email;
-
-public interface EmailSender {
-    void send(String to, String email);
-}
